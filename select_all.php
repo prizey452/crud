b8b55fb4487097180f3a92d499792d920512a5c8
prizey452 <barakamuyonga2002@gmail.com>
2023-06-19 15:20:56 +0300

    Update select_all.php

--- select_all.php
+++ select_all.php
@@ -9,7 +9,7 @@ echo "<table border = '2' >" . "<tr >" . "<th>" . "ID" . "</th>" . "<th>" . "EMA
 while ($line = mysqli_fetch_assoc($action)) {
     echo "<tr>" . "<td>" . $line["id"] . "</td>" . "<td>" . $line["email"] . "</td>" . "<td>" . $line["passcode"] .
         "</td>" . "<td>" . "<form action='update_table.php' method='get'> <button value=$line[id] name='updateid'>UPDATE</button>"
-        . "</td>" . "<td>" . "<form action='delete_from_table.php' method='get'> <button value=$line[id] name='delte'>DELETE</button>" . "</form></td>" . "</tr>";
+        . "</td>" . "<td>" . "</form>" . "<form action='delete_from_table.php' method='get'> <button value=$line[id] name='delte'>DELETE</button>" . "</form></td>" . "</tr>";
 }
 
 echo "</table>";
