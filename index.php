7acef7d4c2e9c1f8547e049aa702b46d3e702056
prizey452 <barakamuyonga2002@gmail.com>
2023-06-20 22:03:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,31 +12,31 @@ $connection = mysqli_connect($server, $username, $password, $database);
 if (!$connection) {
 	die("<h2>check config, is server alive?</h2>" . mysqli_connect_error());
 
-} else {
-	echo "connection success<br>";
 }
 
-
-//CREATING A DATABASE, IF IT DOESNT EXIST
-//$create_database = "CREATE DATABASE USERS";
-
-/*if (mysqli_query($connection, $create_database)) {
-	echo "database created successfully";
-} else {
-	echo "does the database exist?<br>" . mysqli_error($connection);
-}*/
-
 //create table
-$table_create = "CREATE TABLE data(
+echo "<form action='' method='post'>";
+echo "<label> CREATE TABLE</label><br />";
+echo "<input type='text' name='table'" . " />";
+
+echo "<br /><br />";
+echo "<button name='update'>UPDATE</button>";
+echo "</form>";
+
+
+if (isset($_POST['table'])) {
+	$tablename = $_POST['table'];
+	$table_create = "CREATE TABLE $tablename(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(30) NOT NULL,
 	passcode VARCHAR(50) NOT NULL
 )";
 
-if (mysqli_query($connection, $table_create)) {
-	echo "sql success";
-} else {
-	echo "sql error<br>" . mysqli_error($connection);
+	if (mysqli_query($connection, $table_create)) {
+		echo "Table created successfully";
+	} else {
+		echo "sql error<br>" . mysqli_error($connection);
+	}
 }
 
 
